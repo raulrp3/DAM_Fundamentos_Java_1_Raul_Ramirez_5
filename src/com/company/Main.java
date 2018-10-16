89b1a81dd4f0b3db968dba176547bb5490e70727
raulrp3 <raul.ramirez@escuelaestech.es>
2018-10-16 16:03:41 +0200

    Ejercicio 4

---
+++ src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+    public static int validarNumero(Scanner sc) {
+        int numeroValidado;
+        while (!sc.hasNextInt()) {
+            System.out.print("¡Introduce un valor numérico entero! ");
+            sc.next();
+        }
+        numeroValidado = sc.nextInt();
+        return numeroValidado;
+    }
+
+    public static String compararNumeros(int primerNumero, int segundoNumero) {
+        String cadenaResultado;
+        if (primerNumero == segundoNumero) {
+            cadenaResultado = "Los dos números SÍ son iguales.";
+        } else {
+            cadenaResultado = "Los dos números NO son iguales.";
+        }
+        return cadenaResultado;
+    }
+
+    public static void main(String[] args) {
+        int primerNumero, segundoNumero;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Vamos a comparar dos números y comprobar si son iguales o no.");
+        System.out.print("Introduce un primer número: ");
+        primerNumero = validarNumero(sc);
+        System.out.print("Introduce un segundo número: ");
+        segundoNumero = validarNumero(sc);
+        System.out.println(compararNumeros(primerNumero, segundoNumero));
+    }
+}
